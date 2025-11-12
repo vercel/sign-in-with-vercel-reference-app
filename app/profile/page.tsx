10f55bcdd2aab323c4930ddd81547a9720ea3919
Ana Jovanova <ana@vercel.com>
2025-11-12 18:48:00 +0100

    Use /userinfo endpoint

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -6,16 +6,15 @@ import TokenIntrospection from "../components/token-introspection";
 export default async function Profile() {
   const cookieStore = await cookies();
   const token = cookieStore.get("access_token")?.value;
-  const result = await fetch("https://vercel.com/api/v2/user", {
+  const result = await fetch("https://vercel.com/api/login/oauth/userinfo", {
     method: "GET",
     headers: {
       Authorization: `Bearer ${token}`,
     },
   });
   const data = await result.json();
-  const user = data.user;
 
-  if (!user) {
+  if (!data) {
     return (
       <main className="p-10">
         <h1 className="text-3xl font-semibold">Error</h1>
@@ -35,19 +34,19 @@ export default async function Profile() {
     <main className="p-10">
       <h1 className="text-3xl font-semibold">Profile</h1>
       <p className="mt-4">
-        Welcome to your profile page <strong>{user.name}</strong>.
+        Welcome to your profile page <strong>{data.name}</strong>.
       </p>
       <div>
         <h2 className="text-xl font-semibold mt-8">User Details</h2>
         <ul className="mt-4">
           <li>
-            <strong>Name:</strong> {user.name}
+            <strong>Name:</strong> {data.name}
           </li>
           <li>
-            <strong>Email:</strong> {user.email}
+            <strong>Email:</strong> {data.email}
           </li>
           <li>
-            <strong>Username:</strong> {user.username}
+            <strong>Username:</strong> {data.preferred_username}
           </li>
         </ul>
       </div>
@@ -55,7 +54,7 @@ export default async function Profile() {
         Access to the authorized application can be reviewed and revoked in your{" "}
         <Link
           target="_blank"
-          href={`https://vercel.com/account/settings/sign-in-with-vercel/connection_${process.env.NEXT_PUBLIC_CLIENT_ID}_${user.id}`}
+          href={`https://vercel.com/account/settings/sign-in-with-vercel/connection_${process.env.NEXT_PUBLIC_CLIENT_ID}_${data.sub}`}
           className="underline"
         >
           Account Settings
